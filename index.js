92778afe6aec6a7d9ab8348369a54a1fcdea072f
Kailash <87180361+KailashIITBHU@users.noreply.github.com>
2022-06-27 11:42:47 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+function display(val){
+    document.getElementById("textarea").value+=val;
+}
+function calculate(){
+    var val=document.getElementById('textarea').value;
+    var ans=eval(val);
+    document.getElementById('textarea').value=ans;
+}
+function clr(){
+    document.getElementById('textarea').value=''
+}
